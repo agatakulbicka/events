611cc0d0f4d91ae0b05bb284eabc88e5d98bf346
Agata Kulbicka <agata_kulbicka@wp.pl>
2016-11-29 10:48:44 +0100

    displaying on calendar real events from events.json

--- src/events-calendar/EventsCalendar.js
+++ src/events-calendar/EventsCalendar.js
@@ -3,24 +3,36 @@ import BigCalendar from 'react-big-calendar'
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 import moment from 'moment'
 BigCalendar.momentLocalizer(moment);
-import events from './events'
 import {Grid, Row, Col} from 'react-bootstrap'
 import './events-calendar-styles.css'
+import {connect} from 'react-redux'
+
+const mapStateToProps = (state) => ({
+    events: state.eventsCitiesData.events
+})
 
 
-export default (props) =>
-    <div id="mainEventCalendar">
-        <Grid>
-            <Row>
-                <Col>
-                    <BigCalendar
-                        //    {...this.props}
-                        events={events}
-                        defaultDate={new Date()}
-                        defaultView='month'
-                        popup
-                    />
-                </Col>
-            </Row>
-        </Grid>
-    </div>
+class EventCalendar extends React.Component {
+    render(){
+
+        var {events}=this.props;
+        return (
+            <div id="mainEventCalendar">
+                <Grid>
+                    <Row>
+                        <Col>
+                            <BigCalendar
+                                events={events.map((event) => event)}
+                                defaultDate={new Date()}
+                                defaultView='month'
+                                popup
+                            />
+                        </Col>
+                    </Row>
+                </Grid>
+            </div>
+        )
+    }
+}
+
+export default connect (mapStateToProps)(EventCalendar)
